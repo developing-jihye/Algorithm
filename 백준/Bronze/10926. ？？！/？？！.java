9c37d52eae7e3fe90de282740cc1fbdd86119eb3
김지혜 <163363130+developing-jihye@users.noreply.github.com>
2024-06-10 10:22:20 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        String input = in.next();
+        in.close();
+
+        System.out.println(input + "??!");
+    }
+}
