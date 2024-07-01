bafd3fbc358ce2cea5660efd768337e51e88a458
김지혜 <163363130+developing-jihye@users.noreply.github.com>
2024-07-01 19:34:41 +0900

    [Bronze III] Title: 최소, 최대, Time: 1208 ms, Memory: 102600 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,23 +1,24 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String args[]) throws IOException {
 
-        Scanner in = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = in.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
-        int arr[] = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[N];
         for(int i = 0; i < N; i++) {
-            arr[i] = in.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        in.close();
-
         Arrays.sort(arr);
-        System.out.println(arr[0] + " " + arr[N-1]);
 
+        System.out.println(arr[0] + " " + arr[N-1]);
     }
 }
