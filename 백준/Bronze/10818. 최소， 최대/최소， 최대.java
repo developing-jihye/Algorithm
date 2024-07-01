885f68b30e4dcb6a67c7bc4b2de3d4ceb2159b5c
김지혜 <163363130+developing-jihye@users.noreply.github.com>
2024-07-01 19:21:16 +0900

    [Bronze III] Title: 최소, 최대, Time: 2188 ms, Memory: 321340 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        int arr[] = new int[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        in.close();
+
+        Arrays.sort(arr);
+        System.out.println(arr[0] + " " + arr[N-1]);
+
+    }
+}
